f7d19264d67acb60b745a0d610bc191d41d7c100
romaiy <sokolovgit256@gmail.com>
2023-12-14 00:27:06 +0600

    fix: debag fix

--- apps/frontend/src/pages/no-page/index.tsx
+++ apps/frontend/src/pages/no-page/index.tsx
@@ -1,20 +1,25 @@
-import { useEffect } from 'react';
-import { useLocation, useNavigate } from 'react-router-dom';
+import { useEffect, useMemo } from 'react';
+import { useLocation, useNavigate, useSearchParams } from 'react-router-dom';
 import $api from 'shared/api';
 import { MY_SOCIALS_ROUTE } from 'shared/constants/const';
 
 const NoPage = () => {
   const navigate = useNavigate();
-  const location = useLocation();
+
+  let [searchParams, setSearchParams] = useSearchParams();
 
   useEffect(() => {
     const indexToken = location.search.split('%22').indexOf('token') + 2;
     const indexUuid = location.search.split('%22').indexOf('uuid') + 2;
-    $api.post(
-      `/auth/vk?silent_token=${location.search.split('%22')[indexToken]}&uuid=${
-        location.search.split('%22')[indexUuid]
-      }`
+    console.log(
+      location.search.split('%22')[indexToken],
+      location.search.split('%22')[indexUuid]
     );
+    // $api.post(
+    //   `/auth/vk?silent_token=${location.search.split('%22')[indexToken]}&uuid=${
+    //     location.search.split('%22')[indexUuid]
+    //   }`
+    // );
     // .then(() => navigate(MY_SOCIALS_ROUTE));
   }, []);
 
