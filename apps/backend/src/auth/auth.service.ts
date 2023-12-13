bb5e9a2c8a5e207f8c36a3f3093eee72fe6c329b
Isachenko Ivan <vanisachenko@gmail.com>
2023-12-14 00:10:11 +0600

    fix: more logs

--- apps/backend/src/auth/auth.service.ts
+++ apps/backend/src/auth/auth.service.ts
@@ -42,6 +42,8 @@ export class AuthService {
     //const result = await axios.get(uri)
     //Logger.log(result)
 
+    Logger.log(silentToken, ' ', uuid)
+
     const accessuri = `https://api.vk.com/method/auth.exchangeSilentAuthToken?access_token=${clientdata.service_token}&token=${silentToken}&uuid=${uuid}`
 
     Logger.log(accessuri)
