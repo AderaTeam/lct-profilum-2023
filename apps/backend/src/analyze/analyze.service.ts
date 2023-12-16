93cb76340a3281fabaedb92a3d1709800c389f2c
Isachenko Ivan <vanisachenko@gmail.com>
2023-12-17 01:15:34 +0600

    fix: logger

--- apps/backend/src/analyze/analyze.service.ts
+++ apps/backend/src/analyze/analyze.service.ts
@@ -1,4 +1,4 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, Logger } from '@nestjs/common';
 import { UserService } from '../user/user.service';
 import { SocialsService } from '../socials/socials.service';
 import axios from 'axios';
@@ -20,11 +20,13 @@ export class AnalyzeService {
         {
             const userVKid = userSocials.filter((obj) => obj.social.name == 'VK')[0].originaluserid
 
+            Logger.log(userVKid)
             var vkResultWorks = (await axios.get(`http://178.170.192.87:9000/vk/simple_analize_interests/?user_id=${userVKid}&n_of_works=3`)).data
         }  
         if(userSocials.filter((obj) => obj.social.name == 'LeaderID'))
         {
             const leaderID = userSocials.filter((obj) => obj.social.name == 'LeaderID')[0].originaluserid
+            Logger.log(leaderID)
 
             var leaderResultWorks = (await axios.get(`http://178.170.192.87:9000/leaderid/get_works/?user_id=${leaderID}&n_of_works=3`)).data
         }
