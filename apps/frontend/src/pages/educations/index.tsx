9a20c216228ec904c3cc5ef0fe8a64a5fddf026c
romaiy <sokolovgit256@gmail.com>
2023-12-16 20:20:49 +0600

    fix: unvercity widget fix structure

--- apps/frontend/src/pages/educations/index.tsx
+++ apps/frontend/src/pages/educations/index.tsx
@@ -8,12 +8,15 @@ import { Input } from 'shared/components/Input';
 
 import img1 from 'shared/assets/university/1.png';
 import img2 from 'shared/assets/university/2.png';
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import { ResultCard } from 'shared/components/ResultCard';
 
 const EducationsPage = () => {
   const { control, watch } = useForm();
-  const [selectedUniversity, setSelectedUniversity] = useState([]);
+  const [selectedUniversity, setSelectedUniversity] = useState<IUniversity[]>(
+    []
+  );
+  const [university, setUniversity] = useState<IUniversity[]>([]);
 
   const data: IUniversity[] = [
     {
@@ -45,14 +48,49 @@ const EducationsPage = () => {
     },
   ];
 
+  const data2 = [
+    {
+      name: 'Омский Государственный Технический Университет',
+      city: 'Омск',
+      popularity: 'Средняя',
+      students: 5000,
+      budgetPlaces: true,
+      tags: ['ТОП 20', 'ПОДХОДИТ ПОД ВАШ ПУТЬ', 'МИН БАЛЛ - 200'],
+      image: img1,
+    },
+    {
+      name: 'МГУ имени Ломоносова',
+      city: 'Омск',
+      popularity: 'Высокая',
+      students: 5000,
+      budgetPlaces: true,
+      tags: ['ТОП 20', 'ПОДХОДИТ ПОД ВАШ ПУТЬ', 'МИН БАЛЛ - 200'],
+      image: img2,
+    },
+  ];
+
+  const getAllUnivercity = () => {
+    setUniversity(data);
+  };
+
+  const getSelectedUnivercity = () => {
+    setSelectedUniversity(data2);
+  };
+
   const handleSelectUnivercity = (university: IUniversity) => {
     console.log(university);
+    getSelectedUnivercity();
   };
+
+  useEffect(() => {
+    getAllUnivercity();
+    getSelectedUnivercity();
+  }, []);
 
   return (
     <MainWrapper>
       <Stack gap={48}>
-        <EducationsSelectedCard />
+        <EducationsSelectedCard selectedUniversity={selectedUniversity} />
         <Flex gap={32}>
           <Controller
             name="filter"
@@ -63,7 +101,7 @@ const EducationsPage = () => {
         </Flex>
         <EducationsUniversity
           handleSelectUnivercity={handleSelectUnivercity}
-          university={data.filter((item) =>
+          university={university.filter((item) =>
             item.name
               .toLowerCase()
               .includes(watch('filter')?.toLowerCase() || '')
