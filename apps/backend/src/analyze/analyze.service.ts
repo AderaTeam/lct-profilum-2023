26edd644fe8e2db1341f90902759724367f12bc7
Isachenko Ivan <vanisachenko@gmail.com>
2023-12-17 01:54:07 +0600

    fix: -/

--- apps/backend/src/analyze/analyze.service.ts
+++ apps/backend/src/analyze/analyze.service.ts
@@ -21,14 +21,14 @@ export class AnalyzeService {
             const userVKid = userSocials.filter((obj) => obj.social.name == 'VK')[0].originaluserid
 
             Logger.log(userVKid)
-            var vkResultWorks = (await axios.get(`https://py.adera-team.ru/vk/simple_analize_interests/?user_id=${userVKid}&n_of_works=4/`)).data
+            var vkResultWorks = (await axios.get(`https://py.adera-team.ru/vk/simple_analize_interests/?user_id=${userVKid}&n_of_works=4`)).data
         }  
         if(userSocials.filter((obj) => obj.social.name == 'LeaderID'))
         {
             const leaderID = userSocials.filter((obj) => obj.social.name == 'LeaderID')[0].originaluserid
             Logger.log(leaderID)
 
-            var leaderResultWorks = (await axios.get(`http://178.170.192.87:9000/leaderid/get_works/?user_id=${leaderID}&n_of_works=4/`)).data
+            var leaderResultWorks = (await axios.get(`http://178.170.192.87:9000/leaderid/get_works/?user_id=${leaderID}&n_of_works=4`)).data
         }
 
         return {vkres: vkResultWorks, leaderes: leaderResultWorks}
