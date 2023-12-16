999231a2f02adb9ee0501a900f713b5b3dd496dd
Isachenko Ivan <vanisachenko@gmail.com>
2023-12-17 01:10:08 +0600

    fix: url to straight ip

--- apps/backend/src/analyze/analyze.service.ts
+++ apps/backend/src/analyze/analyze.service.ts
@@ -20,13 +20,13 @@ export class AnalyzeService {
         {
             const userVKid = userSocials.filter((obj) => obj.social.name == 'VK')[0].originaluserid
 
-            var vkResultWorks = (await axios.get(`https://py.adera-team.ru/vk/simple_analize_interests/?user_id=${userVKid}&n_of_works=3`)).data
+            var vkResultWorks = (await axios.get(`https://178.170.192.87:9000/vk/simple_analize_interests/?user_id=${userVKid}&n_of_works=3`)).data
         }  
         if(userSocials.filter((obj) => obj.social.name == 'LeaderID'))
         {
             const leaderID = userSocials.filter((obj) => obj.social.name == 'LeaderID')[0].originaluserid
 
-            var leaderResultWorks = (await axios.get(`https://py.adera-team.ru/leaderid/get_works/?user_id=${leaderID}&n_of_works=3`)).data
+            var leaderResultWorks = (await axios.get(`https://178.170.192.87:9000/leaderid/get_works/?user_id=${leaderID}&n_of_works=3`)).data
         }
 
         return {vkres: vkResultWorks, leaderes: leaderResultWorks}
