cba3500463ecb581cbbd398be5b71d5739574d31
Isachenko Ivan <vanisachenko@gmail.com>
2023-12-17 01:43:39 +0600

    fix: uri to https

---
+++ apps/backend/src/user/entities/university.entity.ts
@@ -0,0 +1,71 @@
+import { Entity, PrimaryGeneratedColumn, Column, OneToMany, JoinColumn, JoinTable, ManyToMany, BeforeInsert, BeforeUpdate } from "typeorm"
+import { AchievementOwned } from "../../achievement/entities/achievementOwned.entity"
+import { OwnedPath } from "../../paths/entities/ownedPath.entity"
+import { SocialUsers } from "../../socials/entities/socialsUsers.entity"
+import { Path } from "../../paths/entities/path.entity"
+import { Card } from "../../community/entities/card.entity"
+
+@Entity()
+export class Univercity {
+
+    @PrimaryGeneratedColumn()
+    id: number
+
+    @Column(
+        {
+            nullable: false,
+        }
+    )
+    name: string
+
+    @Column(
+        {
+            nullable: false,
+        }
+    )
+    city: string
+
+    @Column(
+        {
+            nullable: false,
+        }
+    )
+    popularity: string
+
+    @Column(
+        {
+            nullable: false,
+        }
+    )
+    students: number
+
+    @Column(
+        {
+            nullable: false,
+        }
+    )
+    budgetPlaces: boolean
+
+    @Column(
+        {
+            nullable: false,
+            array: true,
+        }
+    )
+    tags: string[]
+
+    @Column(
+        {
+            nullable: false,
+        }
+    )
+    image: string
+
+    @Column(
+        {
+            nullable: false,
+            select: false
+        }
+    )
+    imageBuff: string
+}
