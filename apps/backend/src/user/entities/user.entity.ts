07abe23e527cbd99fb6e59643d7a03c31b0bd73b
Isachenko Ivan <vanisachenko@gmail.com>
2023-12-16 14:23:31 +0600

    fix: z/s finalle

--- apps/backend/src/user/entities/user.entity.ts
+++ apps/backend/src/user/entities/user.entity.ts
@@ -106,7 +106,7 @@ export class User {
 
     @ManyToMany(() => Path, (path) => path.users, {eager: true, onDelete: 'SET NULL'})
     @JoinTable()
-    analyzedPaths: Path[]
+    analysedPaths: Path[]
 
     @ManyToMany(() => Card, (card) => card.author, {onDelete: 'CASCADE'})
     @JoinTable()
