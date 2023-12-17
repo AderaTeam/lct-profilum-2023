c380db2b328f0bc9bf98864649e4f6baa3993385
Mike Tsvirkunov <mike.tsvirkunov@gmail.com>
2023-12-17 23:40:25 +0600

    trycheck

--- apps/algorithms/API/functions/getters/leader_id/get_leader_id_main_user_info.py
+++ apps/algorithms/API/functions/getters/leader_id/get_leader_id_main_user_info.py
@@ -4,6 +4,7 @@ import ioc
 
 
 def mainLeaderIdUserInfo(user_id):
+    print('asdfghjsdfghj\nasdfghsdfghj\nasdfghdfghj')
     main_url = 'https://leader-id.ru/'
     user_suburl = 'users/'
     textNormalization = ioc.require('stdLeaderIdTextNormalization')
@@ -11,10 +12,12 @@ def mainLeaderIdUserInfo(user_id):
     userInfoProcessor = ioc.require('stdLeaderIdMainUserInfoProcessor')
     ms = requests.get(main_url + user_suburl + str(user_id)).content
     soup = BeautifulSoup(ms, 'html.parser').find(class_='page')
+    # print(soup)
     data = dict()
     for i in set([i for i in soup.find_all('h3') if textNormalization(i.text) in usefuleValues]):
         d = userInfoProcessor(i.find_parents())
         if len(d) > len(data.get(textNormalization(i.text), [])):
             print(d)
             data[textNormalization(i.text)] = d 
+    # print('data', data)
     return data
