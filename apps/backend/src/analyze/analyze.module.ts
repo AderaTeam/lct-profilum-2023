d58cf5cd11f26b68f4d5765c2aad9789ac68a337
Isachenko Ivan <vanisachenko@gmail.com>
2023-12-17 20:10:05 +0600

    fix: service<module

--- apps/backend/src/analyze/analyze.module.ts
+++ apps/backend/src/analyze/analyze.module.ts
@@ -3,10 +3,10 @@ import { AnalyzeService } from './analyze.service';
 import { AnalyzeController } from './analyze.controller';
 import { UserModule } from '../user/user.module';
 import { SocialsModule } from '../socials/socials.module';
-import { PathsService } from '../paths/paths.service';
+import { PathsModule } from '../paths/paths.module';
 
 @Module({
-  imports: [UserModule, SocialsModule, PathsService],
+  imports: [UserModule, SocialsModule, PathsModule],
   controllers: [AnalyzeController],
   providers: [AnalyzeService],
 })
